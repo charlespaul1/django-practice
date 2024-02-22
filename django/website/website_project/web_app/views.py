6b92fc49f0e25146ab865dd06c5f6ce26cb8cef1
charlespaul1 <charlespaulwabomba@gmail.com>
2024-02-22 16:02:44 +0300

    added loggin view

--- django/website/website_project/web_app/views.py
+++ django/website/website_project/web_app/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import  authenticate, login, logout
 from django.contrib import messages
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if user is logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # give feedback
+            messages.success(request, 'login successful')
+            # redirect the user to home page
+            return redirect('home')
+        else:
+            # if somethiing goes wrong and user cant login
+            messages.success(request, 'Error logging in, try')
+            return redirect('home')
+        
+    else:
+            
+        return render(request, 'home.html', {})
 
-def login_user(request):
-    pass
+
 
 def logout_user(request):
     pass
